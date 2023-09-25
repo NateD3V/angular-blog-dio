555edce85a40654dd07c616bd8a09cdd1e922124
Nathan <adm.nate@hotmail.com>
2023-09-25 11:25:18 -0300

    created dinamic component property

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css',
+  './home.responsive.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
 
   constructor() { }
 
@@ -13,3 +14,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
